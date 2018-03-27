94e812accb04d048efdf68b176994738ce383803
Hector Romojaro <hector.romojaro@gmail.com>
2018-03-27 14:22:30 +0000

    Fix typos

--- tcl/dotlrn-quota-procs.tcl
+++ tcl/dotlrn-quota-procs.tcl
@@ -72,7 +72,7 @@ namespace eval dotlrn_quota {
     ad_proc -public add_applet_to_community {
         community_id
     } {
-        Add the quota applet to a specifc dotlrn community
+        Add the quota applet to a specific dotlrn community
     } {
         set portal_id [dotlrn_community::get_portal_id -community_id $community_id]
 
@@ -100,7 +100,7 @@ namespace eval dotlrn_quota {
     } {
         remove the applet from the community
     } {
-        ad_return_complaint 1 "[applet_key] remove_applet_from_community not implimented!"
+        ad_return_complaint 1 "[applet_key] remove_applet_from_community not implemented!"
     }
 
     ad_proc -public add_user {
@@ -122,7 +122,7 @@ namespace eval dotlrn_quota {
         community_id
         user_id
     } {
-        Add a user to a specifc dotlrn community
+        Add a user to a specific dotlrn community
     } {
         set package_id [dotlrn_community::get_applet_package_id -community_id $community_id -applet_key [applet_key]]
         set portal_id [dotlrn::get_portal_id -user_id $user_id]
